70fb55b1de658e5b6d150f7d38c8a8d3aeb87f87
aadi8999 <30338118+aadi8999@users.noreply.github.com>
2024-12-18 19:57:08 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,21 @@
 pipeline {
     agent any
-    stage {
-        stage {'Clone Repo'} {
-            sh "export AWS_DEFAULT_REGION=us-east-1"
-            sh "aws cloudformation create-stack --stack-name CloudFormation1 --template-body file://CloudFormation1.yml --region 'us-east-1'"
+
+    environment {
+        AWS_DEFAULT_REGION = 'us-east-1'  // Setting the AWS region as an environment variable
+    }
+
+    stages {
+        stage('Clone Repo') {
+            steps {
+                script {
+                    // Assuming you have a repository to clone
+                    //sh 'git clone https://github.com/your-repo.git'
+                    
+                    // Create a CloudFormation stack
+                    sh 'aws cloudformation create-stack --stack-name CloudFormation1 --template-body file://CloudFormation1.yml --region us-east-1'
+                }
+            }
         }
     }
 }
