b2e5f199593b2ffec098c59c481d9bab17c0929e
aadi8999 <30338118+aadi8999@users.noreply.github.com>
2024-12-18 18:16:08 +0530

    Add files via upload

---
+++ pipeline.groovy
@@ -0,0 +1,9 @@
+pipeline {
+    agent any
+    stage {
+        stage {'Clone Repo'} {
+            sh "export AWS_DEFAULT_REGION=us-east-1"
+            sh "aws cloudformation create-stack --stack-name CloudFormationStack --template-body file://CloudFormationStack.yml --region 'us-east-1'"
+        }
+    }
+}
