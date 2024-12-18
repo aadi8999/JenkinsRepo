b381849f6f186ae120600681b6d3772bc7b20e56
aadi8999 <30338118+aadi8999@users.noreply.github.com>
2024-12-18 19:01:28 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,14 +1,9 @@
 pipeline {
     agent any
-
-    environment {
-        AWS_REGION = 'us-east-1'  
-        INSTANCE_TYPE = 't2.micro' 
-        AMI_ID = 'ami-0e2c8caa4b6378d8c'  
-        KEY_NAME = 'EC2InstanceUsingCloudFormationWithJenkins' 
-        SECURITY_GROUP = 'sg-0dfc65a8a78a0eacd'  
+    stage {
+        stage {'Clone Repo'} {
+            sh "export AWS_DEFAULT_REGION=us-east-1"
+            sh "aws cloudformation create-stack --stack-name CloudFormation1 --template-body file://CloudFormation1.yml --region 'us-east-1'"
+        }
     }
-
-        stage('Launch EC2 Instance') { steps { script { sh """ aws ec2 run-instances \ --region ${AWS_REGION} \ --instance-type ${INSTANCE_TYPE} \ --image-id ${AMI_ID} \ --key-name ${KEY_NAME} \ --security-group-ids ${SECURITY_GROUP} """ } } }
-    
 }
