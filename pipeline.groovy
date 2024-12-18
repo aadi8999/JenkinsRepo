34d8f661a5eadba4a7d4066067e755bcaa08e2e2
aadi8999 <30338118+aadi8999@users.noreply.github.com>
2024-12-18 18:54:27 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -9,6 +9,6 @@ pipeline {
         SECURITY_GROUP = 'sg-0dfc65a8a78a0eacd'  
     }
 
-        stage('Launch EC2 Instance') { steps { script { sh """ aws ec2 run-instances \ --region ${AWS_REGION} \ --instance-type ${INSTANCE_TYPE} \ --image-id ${AMI_ID} \ --key-name ${KEY_NAME} \ --security-group-ids ${SECURITY_GROUP} """ } }
+        stage('Launch EC2 Instance') { steps { script { sh """ aws ec2 run-instances \ --region ${AWS_REGION} \ --instance-type ${INSTANCE_TYPE} \ --image-id ${AMI_ID} \ --key-name ${KEY_NAME} \ --security-group-ids ${SECURITY_GROUP} """ } } }
     
 }
