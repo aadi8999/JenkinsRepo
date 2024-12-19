6d507287710910bf5baa399f2478e7ed7e5a210f
aadi8999 <30338118+aadi8999@users.noreply.github.com>
2024-12-19 18:09:18 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,19 +2,33 @@ pipeline {
     agent any
 
     environment {
-        AWS_DEFAULT_REGION = 'us-east-1'
+        AWS_REGION = 'us-east-1'  // Replace with your desired AWS region
+        INSTANCE_TYPE = 't2.micro'  // Replace with your desired instance type
+        AMI_ID = 'ami-0e2c8caa4b6378d8c'  // Replace with your desired AMI ID
+        KEY_NAME = 'Ubunbtu'  // Replace with your EC2 key pair name
+        SECURITY_GROUP = 'sg-0dfc65a8a78a0eacd'  // Replace with your security group ID
     }
 
     stages {
-        stage('Clone Repo') {
+        stage('Setup AWS CLI') {
             steps {
-                git branch: 'master', url: 'https://github.com/aadi8999/JenkinsRepo.git', credentialsId: 'aadi8999'
+                sh 'sudo apt update'
+                sh 'sudo apt install -y awscli'
             }
         }
 
-        stage('Deploy CloudFormation Stack') {
+        stage('Launch EC2 Instance') {
             steps {
-                sh 'aws cloudformation create-stack --stack-name CloudFormationStack --template-body file://CloudFormation1.yml --region us-east-1'
+                script {
+                    sh """
+                    aws ec2 run-instances \
+                        --region ${AWS_REGION} \
+                        --instance-type ${INSTANCE_TYPE} \
+                        --image-id ${AMI_ID} \
+                        --key-name ${KEY_NAME} \
+                        --security-group-ids ${SECURITY_GROUP}
+                    """
+                }
             }
         }
     }
